5efc620a6446591ae8de03e7553987d0dc6dfd3a
lisa <lcalegari@users.noreply.github.com>
2022-05-20 16:21:10 -0500

    Initial Commit

---
+++ Recess/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recess
+//
+//  Created by Lisa Calegari on 5/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recess/RecessApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecessApp.swift
+//  Recess
+//
+//  Created by Lisa Calegari on 5/20/22.
+//
+
+import SwiftUI
+
+@main
+struct RecessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
